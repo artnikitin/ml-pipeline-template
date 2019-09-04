61a7e35bc14cbc3c1ba02c89c07ef260705f9888
Artem <artnikitin@gmail.com>
2019-09-04 11:57:49 +0300

    change load.sh

--- load.sh
+++ load.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+git remote rm origin
+
 mkdir static
 mkdir static/data
-touch conf.py load.py clean.py transform.py feature.py model.py evaluate.py
 
-#git init
-#echo ".env/" >> .gitignore
-#echo ".idea/" >> .gitignore
-#echo "*.pyc" >> .gitignore
+echo ".env/" >> .gitignore
+echo ".idea/" >> .gitignore
+echo "*.pyc" >> .gitignore
 
+pip install dvc
+dvc init
 
-#cd /path/to/project && /path/to/interpreter/bin/python3.6 /path/to/forecast.py > /path/to/log.txt
-#dt=$(date '+%d/%m/%Y %H:%M:%S');
-#echo "Executed forecast.py successfully on $dt" >> /path/to/log.txt
-#
-#./misfired.sh misfired.txt
+cd ..
+git clone git@github.com:artnikitin/pipeline.git
+cd pipeline
+python setup.py install
+cd ..
+
+mv dvc_project_template ${BASH_ARGV[1]}
